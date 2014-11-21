794c1c4c379fc37a7cf52aca70c6161cca3ce2c5
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-11-20 20:11:03 -0500

    changed to using route shortcut

--- routes/voltage_routes.js
+++ routes/voltage_routes.js
@@ -4,7 +4,11 @@ var controller = require("../controllers/voltage_controller.js");
 var express = require("express");
 var router = express.Router();
 
-router.get("/voltage", controller.getvoltage);
-router.post("/voltage", controller.addvoltage);
+//router.get("/voltage", controller.getvoltage);
+//router.post("/voltage", controller.addvoltage);
+
+router.route("/voltage").
+    get(controller.getvoltage).
+    post(controller.addvoltage);
 
 module.exports = router;
